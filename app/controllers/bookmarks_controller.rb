46ad6a85f0527612c75a025252a347a467c6b3df
Valerie Finsterwald <finsterwald.valerie@gmail.com>
2021-11-11 16:02:02 +0100

    Back End done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params_bookmark)
+    @list = List.find(params[:list_id])
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:movie_id, :comment, :list_id)
+  end
+end
